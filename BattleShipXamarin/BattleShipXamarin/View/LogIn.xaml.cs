0a81230c432362c4bda94fe10ced5e7a531e0863
Andrej Simionenko <andrej.simioneko@gmail.com>
2022-04-27 20:38:38 +0200

    push

--- BattleShipXamarin/BattleShipXamarin/View/LogIn.xaml.cs
+++ BattleShipXamarin/BattleShipXamarin/View/LogIn.xaml.cs
@@ -12,9 +12,87 @@ namespace BattleShipXamarin.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LogIn : ContentPage
     {
+
         public LogIn()
         {
             InitializeComponent();
         }
+
+        enum EntryOption
+        {
+            Register = 0,
+            Returning = 1,
+            Cancel = 2
+        }
+
+
+         void ToggleEntryView(EntryOption option)
+        {
+            switch (option)
+            {
+                case EntryOption.Register:
+                    {
+                        lblWelcome.IsVisible = false;
+                        layoutChoose.IsVisible = false;
+                        layoutLogin.IsVisible = false;
+                        layoutRegister.IsVisible = true;
+                        break;
+                    }
+                case EntryOption.Returning:
+                    {
+                        lblWelcome.IsVisible = false;
+                        layoutChoose.IsVisible = false;
+                        layoutRegister.IsVisible = false;
+                        layoutLogin.IsVisible = true;
+                        break;
+                    }
+                case EntryOption.Cancel:
+                    {
+                        lblWelcome.IsVisible = true;
+                        layoutChoose.IsVisible = true;
+                        layoutRegister.IsVisible = false;
+                        layoutLogin.IsVisible = false;
+                        break;
+                    }
+            }
+        }
+        void OnbtnNewClicked(object sender, EventArgs args)
+        {
+            ToggleEntryView(EntryOption.Register);
+        }
+        void OnbtnReturnClicked(object sender, EventArgs args)
+        {
+            ToggleEntryView(EntryOption.Returning);
+        }
+        void OnbtnCancelLoginClicked(object sender, EventArgs args)
+        {
+            ToggleEntryView(EntryOption.Cancel);
+        }
+        void OnbtnCancelRegisterClicked(object sender, EventArgs args)
+        {
+            ToggleEntryView(EntryOption.Cancel);
+        }
+
+        void OnbtnRegisterClicked(object sender, EventArgs args)
+        {
+            if (string.IsNullOrEmpty(entryFirstName.Text) || string.IsNullOrEmpty(entryLastName.Text) || string.IsNullOrEmpty(entryEmail.Text))
+                App.Current.MainPage.DisplayAlert("Error", "Please supply the required fields.", "Got it");
+            
+                
+        }
+
+        void OnbtnLoginClicked(object sender, EventArgs args)
+        {
+            if (string.IsNullOrEmpty(entryExistingEmail.Text))
+                App.Current.MainPage.DisplayAlert("Error", "Please supply your email.", "Got it");
+            else
+            {
+                btnLogin.IsEnabled = false;
+              
+            }
+        }
     }
-}
+}  
+
+
+   
